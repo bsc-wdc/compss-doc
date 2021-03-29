b752de393faba0fc76834009180ee8998b1b1057
fconejer <francisco.conejero@bsc.es>
2021-03-29 15:13:38 +0200

    Added script to install dependencies on first time.

---
+++ COMPSs_Manuals/install_dependencies.sh
@@ -0,0 +1,9 @@
+python3 -m pip install sphinx \
+                       sphinx-rtd-theme \
+                       myst-parser \
+                       sphinxcontrib-contentui \
+                       sphinx-copybutton \
+                       sphinxcontrib-spelling \
+                       nbsphinx \
+                       sphinxcontrib-svg2pdfconverter \
+                       sphinxcontrib-bibtex==1.0.0
