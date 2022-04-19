6266dbf8028d7d973bbe7b4f542d800b4f483919
fconejer <francisco.conejero@bsc.es>
2022-04-19 16:37:06 +0200

    Removed unused depedency (bibtex)

--- COMPSs_Manuals/install_dependencies.sh
+++ COMPSs_Manuals/install_dependencies.sh
@@ -7,7 +7,6 @@ python3 -m pip install --upgrade "sphinx==4.0.3" \
                        nbsphinx \
                        sphinxcontrib.yt \
                        sphinxcontrib-svg2pdfconverter \
-                       "sphinxcontrib-bibtex==1.0.0" \
                        sphinx-panels \
                        git+https://github.com/executablebooks/sphinx-tabs \
                        git+https://github.com/sphinx-toolbox/sphinx-toolbox@master
