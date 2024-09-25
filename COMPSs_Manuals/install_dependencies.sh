408f9320a4566c20e9678618643baa5c2d825596
fconejer <francisco.conejero@bsc.es>
2024-09-25 16:34:32 +0200

    Added dependencies.

--- COMPSs_Manuals/install_dependencies.sh
+++ COMPSs_Manuals/install_dependencies.sh
@@ -1,6 +1,5 @@
 python3 -m pip install --upgrade \
-                       -r doc/requirements.txt \
-                       --no-deps
+                       -r doc/requirements.txt
 
 echo "---- NOTE ----"
 echo "Install pandoc, imagemagick, librsvg2-bin with your package manager"
