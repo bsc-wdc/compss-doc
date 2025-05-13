d764ff6670fee82306fce9aa74ba21d1459da196
fconejer <francisco.conejero@bsc.es>
2025-05-13 16:34:59 +0200

    Fixed clean script.

--- COMPSs_Manuals/clean.sh
+++ COMPSs_Manuals/clean.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# 1st step: activate the virtual environment
-if [ ! -f "documentation-builder/bin/activate" ]; then
-    echo "Virtual environment not found. Creating..."
-    ./create_venv.sh
-    echo "Virtual environment created: OK"
+# Clean the virtual environment
+if [ -f "documentation-builder/bin/activate" ]; then
+  source documentation-builder/bin/activate
+  make clean
+  deactivate
 fi
-source documentation-builder/bin/activate
-
-make clean
-
-# End step: deactivate environment
-deactivate
 
 rm -rf documentation-builder
 rm -rf build
